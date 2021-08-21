bf2327cdf0daaa89716e8b7c67a0ba247a271825
Vardan Zakaryan <vardan.zakaryan@sflpro.com>
2021-08-21 21:30:32 +0400

    fix

--- src/server/db.js
+++ src/server/db.js
@@ -1,10 +1,10 @@
 const Pool = require("pg").Pool ;
 const pool = new Pool({
-    user: "postgres",
-    password: "12345678", 
-    host: "localhost", 
-    port: 5432,
-    database:"users"
+    user: process.env.POSTGRES_USER ? process.env.POSTGRES_USER : "postgres",
+    password: process.env.POSTGRES_PASSWORD ? process.env.POSTGRES_PASSWORD : "12345678", 
+    host: process.env.POSTGRES_PASSWORD ? process.env.POSTGRES_PASSWORD : "localhost", 
+    port: process.env.POSTGRES_PORT ? process.env.POSTGRES_PORT : 5432,
+    database: process.env.POSTGRES_DATABSE ? process.env.process.env.POSTGRES_DATABSE : "users"
 });
 
 module.exports = pool;
